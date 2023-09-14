2ee9456c6fea41f164386277b365a7e61e1dce64
noppefoxwolf <noppelabs@gmail.com>
2023-09-14 21:15:31 +0900

    update deps

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,8 +10,8 @@ let package = Package(
         .executable(name: "swift-playground", targets: ["swift-playground"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.3"),
-        .package(url: "https://github.com/stencilproject/Stencil", from: "0.14.2"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
+        .package(url: "https://github.com/stencilproject/Stencil", from: "0.15.1"),
         .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.1"),
     ],
     targets: [
